f2b0f21bf63ca983ab53a027c1d25eaabcaeba6b
Elizabeth Oyebade <oyebadeoyinda18@gmail.com>
2023-04-29 19:35:48 -0400

    Next Commit

--- src/components/core-ui/resume.js
+++ src/components/core-ui/resume.js
@@ -4,82 +4,87 @@ import { greetings } from '../../data/portfolio';
 import Particle from "../../helper/particle";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
-import { Container, Row,Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import resumePdf from "../../assets/Oyindamola_Oyebade_Resume.pdf";
 import  Zoom  from 'react-reveal/Zoom';
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import { ThemeContext } from '../../context/themeContext';
 import Responsive from 'react-responsive';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink = "https://raw.githubusercontent.com/ooyebade/Portfolio/main/src/assets/Oyindamola_Oyebade_Resume.pdf";
 
-const Resume = () => {
+function Resume() {
    const [numPages, setNumPages] = useState(null);
    const { theme } = useContext(ThemeContext);
-   const isDesktop = Responsive({ minWidth: 786 });
+   const isDesktop = 786;
    const [width, setWidth] = useState(1200);
-
     useEffect(() => {
         setWidth(window.innerWidth);
     }, []);
+    
+
 
    return (
        <div style={{ backgroundColor: theme.secondary }}>
-           <section className="service-container">
-               <Particle />
-               <Container className="container">
-                   <Zoom left cascade>
-                       <div className="one">
-                           <span className="heading">Resume</span>
-                       </div>
-                   </Zoom>
+               <Container fluid className="service-container">
+                <Particle />
+                    <Row style={{ justifyContent: "center", position: "relative" }}> 
+                        <Zoom left cascade>
+                            <div className="one">
+                                <span className="heading">Resume</span>
+                            </div>
+                        </Zoom>
+                   </Row>
 
-                   <div className='resume-btn'>
-                       {greetings.resumePdf && (
-                           <a
-                               href={resumePdf}
-                               download='resume'
-                               target='_blank'
-                               rel='noreferrer'
-                               style={{ maxWidth: "250px" }}
-                           >
-                               <button className='main-btn'>
-                                   <AiOutlineDownload />
-                                    Download CV
-                               </button>
-                           </a>
-                       )}
-                   </div>
+                   <Row style={{ justifyContent: "center", position: "relative" }}> 
+                        <div className='resume-btn'>
+                            {greetings.resumePdf && (
+                                <a
+                                    href={resumePdf}
+                                    download='resume'
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    style={{ maxWidth: "250px" }}
+                                >
+                                    <button className='main-btn'>
+                                        <AiOutlineDownload />
+                                            Download CV
+                                    </button>
+                                </a>
+                            )}
+                        </div>
+                   </Row>
 
-                   <div>
-                        <Document file={resumeLink} onLoadSuccess={({ numPages }) => setNumPages(numPages)}>
+                   <Row className='resume'>
+                        <Document file={resumeLink} onLoadSuccess={({ numPages }) => setNumPages(numPages)} className="">
                             {Array(numPages).fill().map((_, i) => (
-                            <Page key={`page_${i + 1}`} pageNumber={i + 1} scale={isDesktop ? 1.7 : 0.6} />
+                            <Page key={`page_${i + 1}`} pageNumber={i + 1} scale={width > isDesktop ? 1.7 : 0.6} />
                             ))}
                         </Document>
-                   </div>
+                    </Row>
 
-                   <div className='resume-btn'>
-                       {greetings.resumePdf && (
-                           <a
-                               href={resumePdf}
-                               download='resume'
-                               target='_blank'
-                               rel='noreferrer'
-                               style={{ maxWidth: "250px" }}
-                           >
-                               <button className='main-btn'>
-                                   <AiOutlineDownload />
-                                    Download CV
-                               </button>
-                           </a>
-                       )}
-                   </div>
-
+                   <Row style={{ justifyContent: "center", position: "relative" }}> 
+                        <div className='resume-btn'>
+                            {greetings.resumePdf && (
+                                <a
+                                    href={resumePdf}
+                                    download='resume'
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    style={{ maxWidth: "250px" }}
+                                >
+                                    <button className='main-btn'>
+                                        <AiOutlineDownload />
+                                            Download CV
+                                    </button>
+                                </a>
+                            )}
+                        </div>
+                   </Row>
                </Container>
-           </section>
        </div>
    )
 }
